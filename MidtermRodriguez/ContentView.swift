8b863ffa919f3d53c72b7200ac900eb6457662da
natepow3rs <nrodriguez.bainds@gmail.com>
2026-09-15 18:56:29 +0800

    Initial Commit

---
+++ MidtermRodriguez/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MidtermRodriguez
+//
+//  Created by Mac-LAB on 9/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MidtermRodriguez/MidtermRodriguezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermRodriguezApp.swift
+//  MidtermRodriguez
+//
+//  Created by Mac-LAB on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct MidtermRodriguezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
